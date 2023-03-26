53bac2610ee043589e7b4f3f8eb7cf73b64b34b8
EduardaBorg <116927347+EduardaBorg@users.noreply.github.com>
2023-03-26 15:46:18 -0300

    feat: Added ProductController and ProductRepository classes

---
+++ projeto/src/main/java/com/example/projeto/model/ProductController.java
@@ -0,0 +1,52 @@
+package com.example.projeto.model;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.repository.ProductRepository;
+
+@RestController
+public class ProductController {
+    
+    @Autowired
+    private ProductRepository productRepository;
+    
+    @PostMapping("/products")
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable(value = "id") Long productId, @RequestBody Product productDetails) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException("Product not found for this id :: " + productId));
+
+        product.setName(productDetails.getName());
+        product.setDescription(productDetails.getDescription());
+        product.setPrice(productDetails.getPrice());
+        product.setStock(productDetails.getStock());
+        final Product updatedProduct = productRepository.save(product);
+        return ResponseEntity.ok(updatedProduct);
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+    
+        if (optionalProduct.isPresent()) {
+            productRepository.delete(optionalProduct.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+}
